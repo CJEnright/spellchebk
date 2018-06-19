ebbbe587c7efdb9895efe1f1a4305f9ae3fc9926
CJ Enright <cenrigh@purdue.edu>
2018-06-19 16:15:03 -0500

    restructure and readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BKSpellCheck
-Implementation of a spell checker using BK trees
+Implementation of a spell checker (and corrector) using [BK-trees](https://en.wikipedia.org/wiki/BK-tree).
+
+## Example
+```
+// Start the tree with an arbitrary word
+tree := BKTree{Word: "cat"}
+
+// And add words as needed
+tree.Add("rat")
+tree.Add("dog")
+
+// And query the tree by giving it a string and a tolerence
+res := tree.Search("eog", 1)
+
+// res = [{Word: "dog", Distance: 1}]
+```
+
+lmao, hope I spelled everything here correctly.
--- bkspellcheck.go
+++ bkspellcheck.go
@@ -1,17 +1,22 @@
+// Package bkspellcheck allows for the creation and use of BK-trees
+// meant specifically for spell checking and correction.
 package bkspellcheck
 
-
+// BKTree is an n-ary BK-tree.
 type BKTree struct {
 	Word string
 	Children []BKTree
 	Distance int
 }
 
+// SearchResult are returned by the search function.
 type SearchResult struct {
 	Word string
 	Distance int
 }
 
+// Add inserts a word into the BK-tree.
+// input is the string you want to add to the tree.
 func (t *BKTree) Add(input string) {
 	dist := Distance(t.Word, input)
 
@@ -25,6 +30,9 @@ func (t *BKTree) Add(input string) {
 	t.Children = append(t.Children, BKTree{Word: input, Distance: dist})
 }
 
+// Search queries the tree and returns SearchResult structs of matching words.
+// word is the string to search for.
+// tol is the tolerence to use while searching.
 func (t BKTree) Search(word string, tol int) (found []SearchResult) {
 	dist := Distance(t.Word, word)
 
@@ -41,18 +49,20 @@ func (t BKTree) Search(word string, tol int) (found []SearchResult) {
 	return found
 }
 
-func min(x, y int) int {
+// Min returns the minimun of two ints.
+func Min(x, y int) int {
 	if x < y {
 		return x
 	}
 	return y
 }
 
+// Distance finds the True Damerau–Levenshtein distance between two words.
 func Distance(word1 string, word2 string) int {
 	// True Damerau–Levenshtein distance, but figured i'd keep the func name light
-
 	// Size of our alphabet
 	var da [255]int
+
 	d := make([][]int, len(word1) + 2)
 	for i := range d {
 		d[i] = make([]int, len(word2) + 2)
@@ -88,7 +98,7 @@ func Distance(word1 string, word2 string) int {
 			del := d[i][j + 1] + 1
 			trans := d[k][l] + (i-k-1) + 1 + (j-l-1)
 
-			d[i + 1][j + 1] = min(sub, min(ins, min(del, trans)))
+			d[i + 1][j + 1] = Min(sub, Min(ins, Min(del, trans)))
 		}
 
 		da[word1[i-1]] = i
